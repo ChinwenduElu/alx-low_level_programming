f7dbb7c52ee3003055d8f41cf23a384a411d1a7d
Chinwendu Elu <eluchinwe.com>
2022-09-09 12:18:05 -0700

    variables, if, else and while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 
 /**
-*main - prints numbers of base 16 in lowercase
+*main - prints all possible combinations of single-digit numbers
 *Return: returns 0 on success
 */
 
 int main(void)
 {
 	int n = '0';
-	int hexadecimal = 'a';
 
 	while (n <= '9')
 	{
 		putchar(n);
+		if (n != 9);
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		n++;
-	}
-
-	while (hexadecimal <= 'f')
-	{
-		putchar(hexadecimal);
-		hexadecimal++;
 	}
 
 	putchar('\n');
