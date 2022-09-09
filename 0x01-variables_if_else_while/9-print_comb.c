61abde5f078d5f0e8af302d7844dbd5c42b758eb
Chinwendu Elu <eluchinwe.com>
2022-09-09 12:31:33 -0700

    variables, if, else and while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-*main - prints all possible combinations of single-digit numbers
+*main - print 0-9 separated with commas, using putchar
 *Return: returns 0 on success
 */
 
@@ -12,7 +12,7 @@ int main(void)
 	while (n <= '9')
 	{
 		putchar(n);
-		if (n != 9);
+		if (n != '9')
 		{
 			putchar(',');
 			putchar(' ');
@@ -21,6 +21,5 @@ int main(void)
 	}
 
 	putchar('\n');
-
 	return (0);
 }
