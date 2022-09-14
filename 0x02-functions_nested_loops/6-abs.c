a4004c89e6994fda13d7d7a3541d47e76d82ed33
Chinwendu Elu <eluchinwe.com>
2022-09-14 06:31:22 -0700

    functions and nestedloops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_abs - checks for alphabetic character
+*Return: an absolute value
+*/
+
+int _abs(int)
+
+{
+	int n;
+	{
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
+
+	return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -46,4 +46,11 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
+/**
+*_bs - checks for alphabetic character
+*Return: an absolute value
+*/
+
+int _abs(int);
+
 #endif
