40c4dc9da15944884ed7ced5dbabb239ab3fdf2f
Chinwendu Elu <eluchinwe.com>
2022-09-15 13:25:24 -0700

    more loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,22 +2,22 @@
 
 /**
 * print_diagonal - prints diagonal line n times.
-*  @n: times line is printes.
+* @n: times line is printes.
 */
 
 void print_diagonal(int n)
 {
 	int x, y;
 
-	for (x = 0; x < n; i++)
+	for (x = '0'; x < n; x++)
 	{
-		for (y = 0; y < n; y++)
+		for (y = '0'; y < n; y++)
 		{
 			_putchar(' ');
 		}
 		_putchar(92);
 
-		if (i < (n - 1))
+		if (x < (n - 1))
 			_putchar('\n');
 	}
 	_putchar('\n');
