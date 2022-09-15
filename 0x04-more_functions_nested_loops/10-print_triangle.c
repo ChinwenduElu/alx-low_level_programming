343e9461fb2d00991a481ec4bdc90c6ecb0cec76
Chinwendu Elu <eluchinwe.com>
2022-09-15 13:41:19 -0700

    more loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,13 +15,13 @@ void print_triangle(int size)
 		_putchar('\n');
 
 	for (height = 1; height <= size; height++)
-		{
-			for (width = 1; width <= (size - height); width++)
-				_putchar(' ');
+	{
+		for (width = 1; width <= (size - height); width++)
+			_putchar(' ');
 
-			for (draw = 1; draw <= height; draw++)
-				_putchar('#');
+		for (draw = 1; draw <= height; draw++)
+			_putchar('#');
 
-			_putchar('\n');
-		}
+		_putchar('\n');
+	}
 }
