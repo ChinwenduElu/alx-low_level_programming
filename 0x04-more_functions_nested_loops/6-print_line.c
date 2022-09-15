345567dcb2dccbcb0d027b5c8bcb01ec052c80a4
Chinwendu Elu <eluchinwe.com>
2022-09-15 13:51:59 -0700

    more loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,8 +11,10 @@ void print_line(int n)
 
 	for (num = n; num > '0'; num--)
 		_putchar('_');
-		_putchar('\n');
 
-	if (n <= '0')
-	_putchar('\n');
+	{
+		if (n <= '0')
+		_putchar('\n');
+	}
+	_putchar('\n')
 }
