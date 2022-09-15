777a105930b6ce2df05ed7009ec589346bd13477
Chinwendu Elu <eluchinwe.com>
2022-09-15 12:27:04 -0700

    more loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,49 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* _putchar - puts character to standard output
+* @c: character to put to standard output
+*/
+
+int _putchar(char c);
+
+/**
+* _isupper - checks for uppercase character
+* @c: character to be checked
+* Return: 1 if true, 0 if false
+*/
+
+int _isupper(int c);
+
+
+/**_isdigit - checks for last digit
+*@c: character
+* Return: 1 if true, 0 if false
+*/
+
+
+int _isdigit(int c);
+
+
+/**
+*mul - multiplies two integers
+* @a: integer 1
+* @b: integer 2
+* Return: product
+*/
+
+
+int mul(int a, int b);
+
+/**
+ *
+ * *print_numbers- prints numbers 0 - 9
+ *
+ * */
+
+
+void print_numbers(void);
+
+
+#endif
