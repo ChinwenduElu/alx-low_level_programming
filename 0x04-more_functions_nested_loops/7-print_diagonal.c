3e527e97119da881ee3d5028e271e1ec609067b2
Chinwendu Elu <eluchinwe.com>
2022-09-16 03:31:08 -0700

    more loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,18 +6,24 @@
 */
 
 void print_diagonal(int n)
-{
-	int i, j;
 
-	for (i = 0; i < n; i++)
+{
+	void print_diagonal(int n)
 	{
-		for (j = 0; j < i; j++)
+		int draw;
+		int space;
+
+		if (n > 0)
 		{
-			_putchar(' ');
+			for (draw = 1; draw <= n; draw++)
+			{
+				for (space = 1; space < draw; space++)
+					_putchar(' ');
+					_putchar('\');
+					_putchar('\n');
+			}
 		}
-		_putchar(92);
-		if (i < (n - 1))
+		else
 			_putchar('\n');
 	}
-	_putchar('\n');
 }
