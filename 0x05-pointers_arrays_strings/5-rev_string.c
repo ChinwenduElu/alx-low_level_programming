7b326d13b8a9b9f310367d2224e9e3dd71fb4388
Chinwendu Elu <eluchinwe.com>
2022-09-19 07:51:08 -0700

    pointers, arrays and srings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,11 +11,11 @@ void rev_string(char *s)
 
 	while (count >= 0)
 	{
-		if (*s[count] == '\0')
+		if (s[count] == '\0')
 			break;
 		count++;
 	}
 	for (count--; count >= 0; count--)
-		_putchar(*s[count]);
+		_putchar(s[count]);
 	_putchar('\n');
 }
