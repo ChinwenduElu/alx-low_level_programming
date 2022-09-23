18f23ca8fb408ba01361b6b8065f2c3476964851
Chinwendu Elu <eluchinwe.com>
2022-09-23 00:53:56 -0700

    pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,12 +8,12 @@
 
 char *cap_string(char *s)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int count = 0, j;
+	int characters[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
-		count++;
+	count++;
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 13; i++)
@@ -21,11 +21,11 @@ char *cap_string(char *s)
 			if (*(s + count) == sep_words[i])
 			{
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - ',';
-						break;
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+					break;
 			}
 		}
 		count++;
-	}
+		}
 	return (s);
 }
