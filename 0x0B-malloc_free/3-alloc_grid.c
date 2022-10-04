ee3b54011a657b26aaa98a49e6c20eec6f15a295
Chinwendu Elu <eluchinwe.com>
2022-10-03 20:38:11 -0700

    mallocs

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,6 +28,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		gridout[i] = malloc(width * sizeof(int));
+
 		if (gridout[i] == NULL)
 		{
 			for (i = 0; i < height; i++)
