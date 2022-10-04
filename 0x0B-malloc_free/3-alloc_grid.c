7aef821171d1fe71c4ab38a2e393a7c427baf7e7
Chinwendu Elu <eluchinwe.com>
2022-10-03 20:36:52 -0700

    mallocs

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,13 +16,15 @@ int **alloc_grid(int width, int height)
 
 	if (width < 1 || height < 1)
 		return (NULL);
+
 	gridout = malloc(height * sizeof(int *));
 
 	if (gridout == NULL)
 	{
 		free(gridout);
-			return (NULL);
+		return (NULL);
 	}
+
 	for (i = 0; i < height; i++)
 	{
 		gridout[i] = malloc(width * sizeof(int));
@@ -30,11 +32,12 @@ int **alloc_grid(int width, int height)
 		{
 			for (i = 0; i < height; i++)
 				free(gridout[i]);
-				free(gridout);
+			free(gridout);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
 			gridout[i][j] = 0;
 	}
+
 	return (gridout);
 }
