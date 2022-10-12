1f7039d036235fc0a7f60cc0286aa7149c115e54
Chinwendu Elu <eluchinwe.com>
2022-10-12 14:36:27 -0700

    3rd changes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,12 @@
 
 /**
 * print_name - prints a name
-*@name: name
-*@f: pointer to function print_name
+* @name: name
+* @f: pointer to function print_name
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+if (name && f)
+	f(name);
 }
