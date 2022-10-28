9fbb798cb1f4edaed038f00892aec8ff2a7e1424
Chinwendu Elu <eluchinwe.com>
2022-10-28 10:47:39 -0700

    corrections

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,27 +12,26 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int len = 0, i;
 	unsigned int ui = 0;
+	int len, base_two;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
+	for (len = 0; b[len] != '\0'; len++)
+		;
 
-	i = 0;
-
-	while (b[i])
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (ui);
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
 
-		if (b[i] == '1')
-			ui += (1 * (1 << len));
-		i++;
-		len--;
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
 	return (ui);
 }
