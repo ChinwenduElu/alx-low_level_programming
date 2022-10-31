fca168c8ad6ae5a5784357f1ff9aeb7f28ea4b87
Chinwendu Elu <eluchinwe.com>
2022-10-31 15:23:44 -0700

    minor changes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,10 +12,10 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t n_rd, n_wr;
+	ssize_t nrd, nwr;
 	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
@@ -23,14 +23,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	buf = malloc(sizeof(char) * (letters));
+
+	if (!buf)
 		return (0);
 
-	n_rd = read(fd, buf, letters);
-	n_wr = write(STDOUT_FILENO, buf, n_rd);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 	free(buf);
-	return (n_rd);
+
+	return (nwr);
 }
